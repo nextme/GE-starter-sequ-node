81ca9edaa2b1def13603958399ab6db12050bf9b
Lu4ezar 'nextme' Ermilove <inextmei@gmail.com>
2020-03-31 19:47:01 +0300

    Removes users routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Geteat api v.1.0.0' });
 });
 
-/* GET home page. */
-router.get('/users', function(req, res, next) {
-  res.render('index', { title: ' 0' });
-});
 module.exports = router;
